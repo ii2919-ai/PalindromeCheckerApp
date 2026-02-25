db622f3eb2bee85b0215ba5adaee509b4a224f5b
ii2919-ai <ii2919@srmist.edu.in>
2026-02-25 10:08:56 +0530

    your_name Added UsecaseName

--- App/src/PalindromeChecker.java
+++ App/src/PalindromeChecker.java
@@ -1,5 +1,16 @@
 public class PalindromeChecker {
-    public static void main(String []args){
-        System.out.println("Hello World");
+    public static void main(String[] args) {
+        String original = "madam";
+        String reversed = "";
+
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed += original.charAt(i);
+        }
+
+        if (original.equals(reversed)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
     }
 }
